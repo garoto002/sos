47f7f77febe058c8ce1dd6ccf784788f7153a555
garoto002 <dgege1993@gmail.com>
2025-08-14 12:40:05 +0200

    Fix dynamic route configuration for PDF generation

--- app/api/suppliers/pdf/route.js
+++ app/api/suppliers/pdf/route.js
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic'
+
 import { NextResponse } from 'next/server';
 import connectToDB from '../../../../utils/DAO';
 import Supplier from '../../../../models/supplierModel';
--- app/api/users/pdf/route.js
+++ app/api/users/pdf/route.js
@@ -1,3 +1,6 @@
+export const dynamic = 'force-dynamic'
+
+
 import { NextResponse } from 'next/server';
 import connectToDB from '../../../../utils/DAO';
 import User from '../../../../models/userModel';
